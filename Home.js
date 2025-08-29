24f42e322c5fd7b61c60e8341ca7bea00821acf6
Manavj0413 <jadhavmanav0@gmail.com>
2025-08-29 18:30:37 +0530

    Initial commit

---
+++ Home.js
@@ -0,0 +1,14 @@
+// const btn = document.getElementById("backToTop");
+//  // Show button when scrolled down
+//     window.onscroll = function() {
+//       if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+//         btn.style.display = "block";
+//       } else {
+//         btn.style.display = "none";
+//       }
+//     };
+
+//     // Scroll to top when clicked
+//     btn.onclick = function() {
+//       window.scrollTo({ top: 0, behavior: 'smooth' });
+//     };
